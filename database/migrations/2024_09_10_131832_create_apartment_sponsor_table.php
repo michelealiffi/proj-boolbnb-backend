376801ef0b8db13d8f7e614106c65130b03351f8
michelealiffi <michelealiffi890@gmail.com>
2024-09-10 15:44:26 +0200

    [ADD] Create migration for apartment_sponsor

---
+++ database/migrations/2024_09_10_131832_create_apartment_sponsor_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartment_sponsor', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('apartment_id')->constrained()->onDelete('cascade');
+            $table->foreignId('sponsor_id')->constrained()->onDelete('cascade');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartment_sponsor');
+    }
+};
