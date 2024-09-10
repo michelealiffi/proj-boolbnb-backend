1bc727d108bf7c7f67002a8e8cf8e11b4583bcde
michelealiffi <michelealiffi890@gmail.com>
2024-09-10 12:42:09 +0200

    [UPD] slug is unique now

--- database/migrations/2024_09_09_145642_create_apartments_table.php
+++ database/migrations/2024_09_09_145642_create_apartments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('apartments', function (Blueprint $table) {
             $table->id();
             $table->string('title', 230);
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->text('description');
             $table->integer('price');
             $table->unsignedTinyInteger('rooms');
