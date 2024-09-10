0417cff07e74fbbd96897bb3f4293e624cff2fa0
michelealiffi <michelealiffi890@gmail.com>
2024-09-10 12:49:09 +0200

    [ADD] Create ApartmentController and add all CRUD and Slug

---
+++ app/Http/Controllers/ApartmentController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Apartment;
+use Illuminate\Http\Request;
+
+class ApartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $apartments = Apartment::all();
+        return view('apartments.index', compact('apartments'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('apartments.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:230',
+            'description' => 'required|string',
+            'price' => 'required|integer|min:0',
+            'rooms' => 'required|integer|min:1|max:255',
+            'bathrooms' => 'required|integer|min:1|max:255',
+            'square_meters' => 'required|integer|min:1',
+            'address' => 'required|string|max:500',
+            'latitude' => 'required|numeric|between:-90,90',
+            'longitude' => 'required|numeric|between:-180,180',
+            'image' => 'nullable|string|max:2048',
+            'is_visible' => 'boolean',
+        ]);
+
+        Apartment::create($request->all());
+
+        return redirect()->route('apartments.index')->with('success', 'Apartment created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($slug)
+    {
+        $apartment = Apartment::where('slug', $slug)->firstOrFail();
+        return view('apartments.show', compact('apartment'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Apartment $apartment)
+    {
+        return view('apartments.edit', compact('apartment'));
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Apartment $apartment)
+    {
+        $request->validate([
+            'title' => 'required|string|max:230',
+            'description' => 'required|string',
+            'price' => 'required|integer|min:0',
+            'rooms' => 'required|integer|min:1|max:255',
+            'bathrooms' => 'required|integer|min:1|max:255',
+            'square_meters' => 'required|integer|min:1',
+            'address' => 'required|string|max:500',
+            'latitude' => 'required|numeric|between:-90,90',
+            'longitude' => 'required|numeric|between:-180,180',
+            'image' => 'nullable|string|max:2048',
+            'is_visible' => 'boolean',
+        ]);
+
+        $apartment->update($request->all());
+
+        return redirect()->route('apartments.index')->with('success', 'Apartment updated successfully.');
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Apartment $apartment)
+    {
+        $apartment->delete();
+
+        return redirect()->route('apartments.index')->with('success', 'Apartment deleted successfully.');
+    }
+}
