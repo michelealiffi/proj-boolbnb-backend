a75d57feea6329cf97a9792b99e1547df51e7af0
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-09-10 15:13:08 +0200

    [UPD] sponsor seeder

--- database/seeders/SponsorSeeder.php
+++ database/seeders/SponsorSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Sponsor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,8 +11,34 @@ class SponsorSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+
+        $seedingData = [
+            [
+                'title' => 'Base',
+                'duration' => 24,
+                'price' => 2.99
+            ],
+            [
+                'title' => 'Intermedio',
+                'duration' => 72,
+                'price' => 5.99
+            ],
+            [
+                'title' => 'Pro',
+                'duration' => 144,
+                'price' => 9.99
+            ]
+        ];
+
+        foreach ($seedingData as $data) {
+
+            $sponsor = new Sponsor();
+            $sponsor->title = $data['title'];
+            $sponsor->duration = $data['duration'];
+            $sponsor->price = $data['price'];
+            $sponsor->save();
+        }
     }
 }
