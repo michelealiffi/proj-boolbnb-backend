83f2b5a67b1e0335aa1284271b00abf8f07c5c00
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-09-11 10:03:05 +0200

    [UPD] added automatci calls to seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(UserSeeder::class);
+        $this->call(ApartmentSeeder::class);
+        $this->call(MessageSeeder::class);
+        $this->call(ServiceSeeder::class);
+        $this->call(VisitSeeder::class);
+        $this->call(SponsorSeeder::class);
+        $this->call(ApartmentServiceSeeder::class);
+        $this->call(ApartmentSponsorSeeder::class);
     }
 }
