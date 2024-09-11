472058256c7ad6bfb9ec6f2337692bf8fd26ca1d
michelealiffi <michelealiffi890@gmail.com>
2024-09-11 12:05:32 +0200

    [UPD] update with icon bnb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,11 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Icons -->
+    <link rel="icon" sizes="192x192"
+        href="https://a0.muscache.com/airbnb/static/icons/android-icon-192x192-c0465f9f0380893768972a31a614b670.png">
+
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
