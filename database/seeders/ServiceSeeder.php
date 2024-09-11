7148b1942876298b49699f89aff487bc0d5a9a27
MiriamPecora <miriampecora97@gmail.com>
2024-09-11 15:55:01 +0200

    [UPD] Updated show view to see apartments in detail and book them (wip), modified service seeder (icon names) and added a relation to user in apartment.php to display name of whom created the apartment in show/views

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -14,12 +14,12 @@ class ServiceSeeder extends Seeder
     public function run(): void
     {
         DB::table('services')->insert([
-            ['name' => 'Wi-Fi', 'icon_name' => 'fas fa-wifi'],
-            ['name' => 'Parcheggio', 'icon_name' => 'fas fa-parking'],
-            ['name' => 'Piscina', 'icon_name' => 'fas fa-swimming-pool'],
-            ['name' => 'Climatizzazione', 'icon_name' => 'fas fa-snowflake'],
-            ['name' => 'Colazione Inclusa', 'icon_name' => 'fas fa-coffee'],
-            ['name' => 'Animali Ammessi', 'icon_name' => 'fas fa-paw'],
+            ['name' => 'Wi-Fi', 'icon_name' => 'fa-solid fa-wifi'],
+            ['name' => 'Parcheggio', 'icon_name' => 'fa-solid fa-parking'],
+            ['name' => 'Piscina', 'icon_name' => 'fa-solid fa-swimming-pool'],
+            ['name' => 'Climatizzazione', 'icon_name' => 'fa-solid fa-snowflake'],
+            ['name' => 'Colazione Inclusa', 'icon_name' => 'fa-solid fa-coffee'],
+            ['name' => 'Animali Ammessi', 'icon_name' => 'fa-solid fa-paw'],
         ]);
     }
 }
