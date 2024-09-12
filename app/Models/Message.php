68f9ac8c9aedd9e4bec26dfd1f20d47ab33bb5ff
michelealiffi <michelealiffi890@gmail.com>
2024-09-12 16:55:43 +0200

    [UPD] update model for controllor

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
 
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['email', 'content', 'apartment_id'];
+
+    public function apartment()
+    {
+        return $this->belongsTo(Apartment::class);
+    }
 }
