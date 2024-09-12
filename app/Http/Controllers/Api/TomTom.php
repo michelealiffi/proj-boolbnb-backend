3e37131cde86007ba5b3a6947ca2596854fabc42
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-09-12 15:13:25 +0200

    [ADD] autocomplete controller method

--- app/Http/Controllers/Api/TomTom.php
+++ app/Http/Controllers/Api/TomTom.php
@@ -16,7 +16,7 @@ class TomTom extends Controller
             'endpoint' => 'https://api.tomtom.com/search/2/geocode',
             'query' => $search,
             'ext' => 'json',
-            'myKey' => '58u54719BrXh6dESJAVByPbU1IJGog0s',
+            'myKey' => env('TOM_TOM_KEY'),
         ])->withoutVerifying()->get('{+endpoint}/{query}.{ext}?key={myKey}');
 
         $data = json_decode($response->body());
@@ -24,6 +24,24 @@ class TomTom extends Controller
         return json_encode([
             'result' => 'ok',
             'data' => $data->results[0],
+        ]);
+    }
+
+    public function autocomplete($search)
+    {
+
+        $response = Http::withUrlParameters([
+            'endpoint' => 'https://api.tomtom.com/search/2/search',
+            'query' => $search,
+            'ext' => 'json',
+            'myKey' => env('TOM_TOM_KEY'),
+        ])->withoutVerifying()->get('{+endpoint}/{query}.{ext}?key={myKey}&typehead=true&limit=10');
+
+        $data = json_decode($response->body());
+
+        return json_encode([
+            'result' => 'ok',
+            'data' => $data,
         ]);
     }
 }
