20f81738a1e0c129e953d578e4b5d91126a8dd60
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-09-13 12:15:02 +0200

    [UPD] moved routes inside auth middleware and removed unwanted routes

--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::resource('apartments', ApartmentController::class)->parameters(['apartments' => 'apartment:slug']);
-
-Route::resource('messages', MessageController::class);
-
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('home');
+
+    // ROTTE APPARTAMENTO
+    Route::resource('apartments', ApartmentController::class)->parameters(['apartments' => 'apartment:slug']);
+
+    // ROTTE PROFILO
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // ROTTE MESSAGGI
+    Route::get('/messages', [MessageController::class, 'index'])->name('messages.index');
+    Route::get('/messages/{message}', [MessageController::class, 'show'])->name('messages.show');
+    Route::DELETE('/messages/{message}', [MessageController::class, 'destroy'])->name('messages.destroy');
 });
 
 require __DIR__ . '/auth.php';
