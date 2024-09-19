cde5c83c525a45d1319f9aba0faf3767311c6938
michelealiffi <michelealiffi890@gmail.com>
2024-09-19 14:20:16 +0200

    [FIX] fix title dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,7 @@
 @extends('layouts.app')
+
+@section('title', 'Boolbnb')
+
 @section('content')
     <div class="jumbotron p-5 mb-4 bg-light rounded-3">
         <div class="container py-5 text-center">
