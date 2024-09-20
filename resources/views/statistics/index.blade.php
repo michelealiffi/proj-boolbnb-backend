fe853644862f61344adf1c751e5cfbfe774a0e78
MiriamPecora <miriampecora97@gmail.com>
2024-09-20 16:54:40 +0200

    [UPD] Restyled statistic page by adding images and changing btn's color

--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -4,11 +4,11 @@
 
 @section('content')
     <div class="container">
-        <h1 class="py-3">Statistiche degli Appartamenti</h1>
-        <table class="table">
+        <h1 class="py-4">Statistiche degli Appartamenti</h1>
+        <table class="table text-center">
             <thead>
                 <tr>
-                    <th>Nome Appartamento</th>
+                    <th>Appartamento</th>
                     <th>Totale Visualizzazioni</th>
                     <th>Totale Messaggi</th>
                     <th>Azioni</th>
@@ -16,12 +16,12 @@
             </thead>
             <tbody>
                 @foreach ($apartments as $apartment)
-                    <tr>
-                        <td>{{ $apartment->title }}</td>
+                    <tr class="align-middle">
+                        <td class="d-flex gap-3 align-items-center"><img src="{{ $apartment->image }}" alt="">{{ $apartment->title }}</td>
                         <td>{{ $apartment->visits_count }}</td>
                         <td>{{ $apartment->messages_count }}</td>
                         <td>
-                            <a href="{{ route('statistics.show', $apartment->id) }}" class="btn btn-primary">Visualizza
+                            <a href="{{ route('statistics.show', $apartment->id) }}" class="py-1 button_magenta">Visualizza
                                 Statistiche</a>
                         </td>
                     </tr>
@@ -30,3 +30,22 @@
         </table>
     </div>
 @endsection
+
+<style scoped>
+    .button_magenta {
+        background-color: #ff385c;
+        color: white;
+        border: none;
+        padding: 15px;
+        border-radius: 5px;
+        cursor: pointer;
+        width: 35%;
+        height: 55px;
+    }
+
+    img {
+        width: 80px;
+        max-height: 80px;
+        border-radius: 5px
+    }
+</style>
