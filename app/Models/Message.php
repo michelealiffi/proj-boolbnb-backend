8a387f84cff87f0edbdc41df8517a214a1d3eb50
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-09-20 09:19:44 +0200

    [UPD] added name to fillable in message

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -10,7 +10,7 @@ class Message extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['email', 'content', 'apartment_id'];
+    protected $fillable = ['email', 'name', 'content', 'apartment_id'];
 
     public function apartment()
     {
