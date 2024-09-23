6f478279619d4f098e9e4b7f7d3bff62d0df5498
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-09-23 20:34:57 +0200

    [UPD] fixed mesages responsivness to column when on mobile

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -11,34 +11,38 @@
                 {{ session('success') }}
             </div>
         @endif
-            <div class="row">
-                @foreach ($messages as $message)
-                    <div class="col-6 my-4">
-                        <div id="message_box" class="rounded py-3 px-4">
-                            <div class="d-flex justify-content-between pt-3">
-                                <div>
-                                    <span style="color: #e91e63;"><strong>{{ $message->name }}</strong> </span> - <span>{{ $message->email }}</span>
-                                </div>
-                                <div>
-                                    <a href="{{ route('messages.show', [$message->apartment->slug, $message->id]) }}"
-                                        class="me-2 text-decoration-none" style="color:rgba(42, 130, 212, 0.89)">
-                                        <i class="fa-solid fa-eye"></i>
-                                    </a>
-                                    <form action="{{ route('messages.destroy', $message->id) }}" method="POST"
-                                        style="display:inline-block;">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn p-0 text-decoration-none pb-1" style="background:none; border:none; color:#8f1505d0;">
-                                            <i class="fa-solid fa-trash"></i>
-                                        </button>
-                                    </form>
-                                </div>
+        <div class="row row-cols-1 row-cols-md-2">
+            @foreach ($messages as $message)
+                <div class="col my-4">
+                    <div id="message_box" class="rounded py-3 px-4">
+                        <div class="d-flex justify-content-between pt-3">
+                            <div>
+                                <span style="color: #e91e63;"><strong>{{ $message->name }}</strong> </span> -
+                                <span>{{ $message->email }}</span>
                             </div>
-                            <p>Ricevuto il <span class="fst-italic">{{ $message->created_at->format('d/m/Y') }}</span>, alle <span class="fst-italic">{{ $message->created_at->format('H:i') }}</span> </p>     
+                            <div>
+                                <a href="{{ route('messages.show', [$message->apartment->slug, $message->id]) }}"
+                                    class="me-2 text-decoration-none" style="color:rgba(42, 130, 212, 0.89)">
+                                    <i class="fa-solid fa-eye"></i>
+                                </a>
+                                <form action="{{ route('messages.destroy', $message->id) }}" method="POST"
+                                    style="display:inline-block;">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn p-0 text-decoration-none pb-1"
+                                        style="background:none; border:none; color:#8f1505d0;">
+                                        <i class="fa-solid fa-trash"></i>
+                                    </button>
+                                </form>
+                            </div>
                         </div>
+                        <p>Ricevuto il <span class="fst-italic">{{ $message->created_at->format('d/m/Y') }}</span>, alle
+                            <span class="fst-italic">{{ $message->created_at->format('H:i') }}</span>
+                        </p>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
 
